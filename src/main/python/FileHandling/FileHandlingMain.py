f96b7008a58f4f3e9507d6bbae661a204b0ed179
Rob Barlow <rwb037@bucknell.edu>
2021-10-21 14:39:04 -0400

    Text Files Added

---
+++ src/main/python/FileHandling/FileHandlingMain.py
@@ -0,0 +1,14 @@
+"""
+*****************************************
+* Name: Rob Barlow
+* Date: 10/21/2021
+* Time: 2:26 PM
+*
+* Project: CSCI311_DNA_Sequencing
+* Class: FileHandlingMain
+*
+* Description: Main file to handle files for the program
+****************************************
+"""
+
+
