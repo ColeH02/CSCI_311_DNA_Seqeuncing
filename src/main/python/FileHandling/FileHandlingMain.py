3ac41d8f27a9b243725270b3c4c24ee166adaffb
Rob Barlow <rwb037@bucknell.edu>
2021-10-25 04:11:13 -0400

    ReadDNA_query completed

--- src/main/python/FileHandling/FileHandlingMain.py
+++ src/main/python/FileHandling/FileHandlingMain.py
@@ -11,4 +11,19 @@
 ****************************************
 """
 
+def ReadDNA_query():
+    """
+    Function to read DNA_query.txt.
 
+    :return: String containing text from DNA_query.txt
+    """
+    DNA_queryRaw = open("DNA_query.txt", "r")
+
+    DNA_queryStr = ""
+    for line in DNA_queryRaw:
+        DNA_queryStr += line
+
+    DNA_queryRaw.close()
+    return DNA_queryStr
+
+ReadDNA_query()
