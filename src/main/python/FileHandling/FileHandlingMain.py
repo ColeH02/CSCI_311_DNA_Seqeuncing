4df299607b00a90776ebdf95f815f95a5dae4ab7
Rob Barlow <rwb037@bucknell.edu>
2021-10-25 04:55:02 -0400

    HandleDNA_sequences finished

--- src/main/python/FileHandling/FileHandlingMain.py
+++ src/main/python/FileHandling/FileHandlingMain.py
@@ -11,19 +11,49 @@
 ****************************************
 """
 
-def ReadDNA_query():
+def ReadFile(file):
     """
     Function to read DNA_query.txt.
 
-    :return: String containing text from DNA_query.txt
+    :param file: name of the file to read in
+    :return: String containing text from file
     """
-    DNA_queryRaw = open("DNA_query.txt", "r")
+    rawFile = open(file, "r") #Opening the file
 
-    DNA_queryStr = ""
-    for line in DNA_queryRaw:
-        DNA_queryStr += line
+    #Reading in each line in the file
+    fileSTR = ""
+    for line in rawFile:
+        fileSTR += line
 
-    DNA_queryRaw.close()
-    return DNA_queryStr
+    rawFile.close()
+    return fileSTR
 
-ReadDNA_query()
+def HandleDNA_sequences(DNA_sequencesStr):
+    """
+    Function that takes a string of known DNA sequences and puts then into a dictionary where key
+    is the sequence description and value is the DNA sequence.
+
+    :param DNA_sequencesStr: String
+    :return: Dictionary
+    """
+    DNA_sequencesList = DNA_sequencesStr.split("\n")
+
+    #Removing any empty elements
+    DNA_sequencesListMod = [line for line in DNA_sequencesList if len(line.strip()) >0]
+    DNA_sequencesDict = {}
+
+    #Iterating over each element and storing the sequences in a dictionary
+    for line in DNA_sequencesListMod:
+        line.strip() #Removing any spaces at the beginning or end
+        if line[0] == ">":
+            key = line
+            DNA_sequencesDict[key] = ""
+        else:
+            DNA_sequencesDict[key] += line.upper()
+
+    return DNA_sequencesDict
+
+
+
+# print(ReadFile("DNA_sequences.txt"))
+# print(HandleDNA_sequences(ReadFile("DNA_sequences.txt")))
